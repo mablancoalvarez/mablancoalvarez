931fa3b1858b6276396d5e3fd3e911e5d09911bd
mablancoalvarez <mablancoalvarez@gmail.com>
2020-05-17 19:26:44 +0200

    add links in social icons

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -4,10 +4,12 @@ const SocialMedia = (props)=> {
 
     return (
     <div className="social-icons"> 
-        <i className="fab fa-linkedin"></i>
-        <i className="fab fa-twitter-square"></i>
-        <i className="fab fa-github"></i>
-        <i className="fas fa-at"></i>
+        <a href='https://www.linkedin.com/in/mariablancoalvarez/'target="_blank" rel="noreferrer noopener"><i className="fab fa-linkedin"></i></a>
+        <a href='https://twitter.com/Marybass'target="_blank" rel="noreferrer noopener"><i className="fab fa-twitter-square"></i></a>
+        <a href='https://github.com/mablancoalvarez'target="_blank" rel="noreferrer noopener">
+        <i className="fab fa-github"></i></a>
+        <a href='mailto:mablancoalvarez@gmail.com'target="_blank" rel="noreferrer noopener">
+        <i className="fas fa-at"></i></a>
     </div>
     );
 }
