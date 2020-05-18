f273b87d7019361779835f2b7d1c151965fa7ce1
mablancoalvarez <mablancoalvarez@gmail.com>
2020-05-18 13:17:17 +0200

    fix margin

--- src/components/AnimatedText.js
+++ src/components/AnimatedText.js
@@ -10,7 +10,7 @@ return (
     </p>
     
     <ul className="content__container__list">
-      <li className="content__container__list__item">Maria!  <span role="img" aria-label="woman tech"> 👩🏻‍💻 </span>
+      <li className="content__container__list__item">Maria!    <span role="img" aria-label="woman tech"> 👩🏻‍💻 </span>
       </li>
       <li className="content__container__list__item"> a Front-End Dev!</li>
       <li className="content__container__list__item">pasionate!  <span role="img" aria-label="yellow heart">💛</span></li>
