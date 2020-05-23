f8da20b62e4e389eac2889359773ce963a63b512
mablancoalvarez <mablancoalvarez@gmail.com>
2020-05-23 19:10:45 +0200

    border-radius image

--- src/works.js
+++ src/works.js
@@ -3,8 +3,28 @@ const works = [
 
     {
         id: "1",
-        name: "Awesome Cards",
+        name: "Portfolio",
         number: "1#",
+        description: "Just this website! The place to show my projects and where you can get to know me better",
+        code:"https://github.com/mablancoalvarez/mablancoalvarez",
+        website:"https://mariablanco.netlify.app/#/projects",
+
+        labels:{
+
+            label1 : "Javascript",
+            label2 : "React",
+            label3 : "HTML5",
+            label4 : "Responsive",
+            label5 : "CSS (Sass)",
+            label6 : "Flexbox",
+        },
+
+        img: './images/sideproject.png',
+    },
+    {
+        id: "2",
+        name: "Awesome Cards",
+        number: "2#",
         description: "Digital Card Generator is a project made with React Js , starting from Vanila Javascript legacy code and using twitter's Rest API. We can customize the look of the card, filling in the data through a form and including dark mode.",
         code:"http://beta.adalab.es/project-promo-i-module-3-team-1-new/#/",
         website:"http://beta.adalab.es/project-promo-i-module-3-team-1-new/#/",
@@ -23,9 +43,9 @@ const works = [
         img: './images/awesome.png',
     },
     {
-        id: "2",
+        id: "3",
         name: "Rick & Morty",
-        number: "2#",
+        number: "3#",
         description: "Responsive web developed with React js, CSS3 & Sass. I access the data info trought an API Rest. Rick & Morty is a project where you can find a specific character or filter by type , furthermore you can more details of each character in a new website route.",
         code:"https://github.com/mablancoalvarez/modulo-3-evaluacion-final-mablancoalvarez",
         website:"http://beta.adalab.es/modulo-3-evaluacion-final-mablancoalvarez/#/",
@@ -39,12 +59,12 @@ const works = [
 
          } ,
 
-        img: './images/project2.png',
+        img: './images/rick.png',
     },
     {
-        id: "3",
+        id: "4",
         name: "Tv Show",
-        number: "3#",
+        number: "4#",
         description: "TV Browser developed with Vainilla Javascript, HTML5,CSS3 & SASS technology and API REST. You can search a Tv Show and you could save in a favorite list list, in addition you can refresh the website without lost your searches.",
         code:"https://github.com/mablancoalvarez/modulo-2-evaluacion-final-mablancoalvarez",
         website:"http://beta.adalab.es/modulo-2-evaluacion-final-mablancoalvarez/?input=game",
@@ -57,12 +77,12 @@ const works = [
             label5 : 'CSS (Sass)',
     },
 
-        img: './images/project3.png',
+        img: './images/tv.png',
     },
     {
-        id: "4",
+        id: "5",
         name: "Collapsing Margins",
-        number: "4#",
+        number: "5#",
         description: "The goal was to create a responsive website with information about our team, according to the client needs, and it features a main page and a second page with a functional contact form.  Website was done using HTLM5 ,CSS3 & SASS and Gulp.",
         code:"https://github.com/mablancoalvarez/project-promo-i-module-1-team-3-afternoon-final",
         website:"http://beta.adalab.es/project-promo-i-module-1-team-3-afternoon-final/",
